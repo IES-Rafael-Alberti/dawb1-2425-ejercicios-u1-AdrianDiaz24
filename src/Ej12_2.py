ed2752c815f3de033028a6ed374466b88d28d0c5
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-17 20:49:35 +0200

    Ej. 1.2.2 v0.2

--- src/Ej12_2.py
+++ src/Ej12_2.py
@@ -1,13 +1,21 @@
-def pedir_horas(horas):
+
+def pedir_horas():
     horas = input("Dime las horas trabajadas: ")
     return horas
 
-def pedir_precio(precio):
+def pedir_precio():
     precio =  input("Dime el precio por hora: ")
     return precio
 
-def main():
+def calcular_importe(horas, precio) -> str:
+    return horas*precio
 
+def main():
+    horas = pedir_horas()
+    precio = pedir_precio()
+    importe = calcular_importe(horas,precio)
+    print("Importe toral: ", importe)
     return 0
-if __name__ == "__main__":
+
+if __name__ =="__main__":
     main()
