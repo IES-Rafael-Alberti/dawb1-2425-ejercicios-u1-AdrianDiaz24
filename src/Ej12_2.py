6681b3e2f9e7bd347d6039c25c3e1300b3c8dbde
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-17 20:16:01 +0200

    Ej. 1.2.2

--- src/Ej12_2.py
+++ src/Ej12_2.py
@@ -1,3 +1,7 @@
 def pedir_horas(horas):
-
+    horas = input("Dime las horas trabajadas: ")
     return horas
+
+def pedir_precio(precio):
+    precio =  input("Dime el precio por hora: ")
+    return precio
