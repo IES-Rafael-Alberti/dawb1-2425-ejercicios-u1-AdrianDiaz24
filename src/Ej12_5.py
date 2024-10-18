5c0e953141d6d8d173b3ee01c5f149f655eee719
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-18 10:52:28 +0200

    Ej. 1.2.5

---
+++ src/Ej12_5.py
@@ -0,0 +1,6 @@
+#Escribe un programa que pida el importe sin IVA de un artículo y el tipo de IVA ~
+#a aplicar y calcule e imprima por pantalla el precio final del artículo.
+
+def pedir_precio():
+    precio = float(input("Dame el precio sin IVA del producto: "))
+    return precio
