a0fccef4d248e8961f7804cf50f00fa8c90249f8
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-18 19:19:36 +0200

    Ej. 1.2.11

--- src/Ej12_11.py
+++ src/Ej12_11.py
@@ -1,9 +1,25 @@
 #Escribir un programa que lea un entero positivo, n, introducido por el usuario y después 
 # muestre en pantalla la suma de todos los enteros desde 1 hasta n. 
-# La suma de los n primeros enteros positivos puede ser calculada de la siguiente forma:
+# La suma de los n primos enteros positivos puede ser calculada de la siguiente forma:
 #suma =((n*(n+1))/2)
 
+def pedir_num():
+    num = float(input("Dame un numero: "))
+    return num
+
+
+def dar_num(a):
+    i = 0
+    while i < a:
+        i = i+1
+        print(i)
+    return i
+
+
+
 def main():
+    a = pedir_num()
+    dar_num(a)
 
     return 0
 
