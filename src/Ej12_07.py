273e2030f8b18cab479db532355f1c481a1058fd
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-18 13:18:01 +0200

    Ej. 1.2.7

---
+++ src/Ej12_07.py
@@ -0,0 +1,20 @@
+#Escribe un programa que solicite tres números al usuario y calcule e imprima por pantalla su suma.
+
+def pedir_num():
+    a = float(print("Dame un numero que sumar: "))
+    b = float(print("Dame otro numero que sumar: "))
+    c = float(print("Dame otro numero que sumar: "))
+    return a,b,c
+
+
+def suma(a,b,c):
+    suma = a+b+c
+    return suma
+
+
+def main():
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
