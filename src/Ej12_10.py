583b38e363fc67c49bdf9dd14b4d770a54a97bb9
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-18 16:34:50 +0200

    Ej.1.2.10

---
+++ src/Ej12_10.py
@@ -0,0 +1,9 @@
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética
+#((3+2)/(2*5))^2
+
+def main():
+    print(((3+2)/(2*5))^2)
+    return 0
+
+if __name__ == "__main__":
+    main()
