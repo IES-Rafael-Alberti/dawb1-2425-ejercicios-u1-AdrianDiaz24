db1a5e694b67b2b1bc587944c673e96725611be9
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-19 00:36:16 +0200

    Ej. 1.2.12

---
+++ src/Ej12_12.py
@@ -0,0 +1,15 @@
+#Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros),
+#  calcule el índice de masa corporal y lo almacene en una variable,
+#  y muestre por pantalla la frase Tu índice de masa corporal es donde es el
+#  índice de masa corporal calculado redondeado con dos decimales.
+
+def pedir_peso():
+    peso = float(input("Dame tu peso en Kg: "))
+    return peso
+
+
+def pedir_altura():
+    altura = float(input("Dame tu altura en metros: "))
+    return altura
+
+
