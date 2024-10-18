96df706024edf6c417ce518608616dcd1a8833ca
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-18 12:17:51 +0200

    Ej. 1.2.6

---
+++ src/Ej12_6.py
@@ -0,0 +1,5 @@
+#Escribe un programa que pida el importe final de un artículo y calcule e imprima por pantalla el IVA 
+#que se ha pagado y el importe sin IVA (suponiendo que se ha aplicado un tipo de IVA del 10%).
+def importe_final():
+    importe_f = float(input("Dime el importe final del producto: "))
+    return importe_f
