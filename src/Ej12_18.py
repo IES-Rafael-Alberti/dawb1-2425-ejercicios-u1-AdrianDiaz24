dd046591192cb3c8506672cdf49b7bbcf8a6c5e9
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-19 20:02:00 +0200

    Ej. 1.2.18

---
+++ src/Ej12_18.py
@@ -0,0 +1,32 @@
+# Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla 
+# el nombre completo del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas 
+# y otra solo con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre 
+# combinando mayúsculas y minúsculas como quiera.
+
+
+def pedir_nombre():
+    nombre = str(input("Dame tu nombre completo: "))
+    return nombre
+
+
+def convertir_min():
+    minuscula = 
+    return minuscula
+
+
+def convertir_mayus():
+    mayusculas = 
+    return mayusculas
+
+
+def convertir_iniciales():
+    iniciales = 
+    return iniciales
+
+
+def main():
+    
+    return
+
+if __name__ == "__main__":
+    main()
