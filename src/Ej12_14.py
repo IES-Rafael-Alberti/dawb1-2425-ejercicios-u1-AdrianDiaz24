f31aa378144eef2e1cbac24f9b587dfe00dfe836
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-19 02:43:37 +0200

    Ej. 1.2.14

---
+++ src/Ej12_14.py
@@ -0,0 +1,20 @@
+# Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas. 
+# Suele hacer venta por correo y la empresa de logística les cobra por peso de 
+# cada paquete así que deben calcular el peso de los payasos y muñecas que 
+# saldrán en cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. 
+# Escribir un programa que lea el número de payasos y muñecas vendidos en el 
+# último pedido y calcule el peso total del paquete que será enviado.
+
+def pedir_muñecas():
+    muñecas = int(print("Dime cuantas muñecas vas a enviar: "))
+    return muñecas
+
+
+def pedir_payaso():
+    payasos = int(print("Dime cuantos payasos vas a enviar: "))
+    return payasos
+
+
+def calcular_peso(payasos,muñecas):
+    peso_t = (payasos*112)+(muñecas*75)
+    return peso_t
