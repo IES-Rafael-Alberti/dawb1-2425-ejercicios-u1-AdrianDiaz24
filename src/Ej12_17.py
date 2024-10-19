31ce3726e0638b0086b2a71c2bfb597cddfc836a
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-19 17:39:03 +0200

    Ej. 1.2.17

--- src/Ej12_17.py
+++ src/Ej12_17.py
@@ -12,13 +12,24 @@ def pedir_num():
     return num
 
 
+def comprobar_nombre(nombre):
+     while nombre <=0 or nombre > 0:
+          nombre = str(input("**ERROR** Esto no es un nombre, por favor introduzca uno valido: "))
+     return nombre
+
+
+def mostrar_nombre(num, nombre):
+        for i in range(num):
+            print(nombre)
+            i = i+1
+        return i
+
+
 def main():
     nombre = pedir_nombre()
     num = pedir_num()
-    i = 0
-
-
-    return 0
+    comprobar_nombre(nombre)
+    mostrar_nombre(num,nombre)
 
 if __name__ == "__main__":
     main()
