c085942a6ece1204fcc6fdea2a0e6d1894352df1
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-19 13:26:34 +0200

    Ej. 1..2.15

--- src/Ej12_15.py
+++ src/Ej12_15.py
@@ -5,3 +5,29 @@
 # cuenta de ahorros, introducida por el usuario. Después el programa debe calcular y 
 # mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. 
 # Redondear cada cantidad a dos decimales.
+
+
+def pedir_dinero():
+    dinero = float(input("Dime cuanto dinero has añadido: "))
+    return dinero
+
+
+
+def calcular_beneficio(dinero):
+    i = 1
+    for i in range(3):
+        beneficio = (dinero*1.04)*i
+        print(f"El dinero generado el {i}º año es de {round(beneficio, 2)}")
+        i = i+1
+    return 0
+
+
+def main():
+    dinero = pedir_dinero()
+    calcular_beneficio(dinero)
+
+    return 
+
+if __name__ == "__main__":
+    main()
+
