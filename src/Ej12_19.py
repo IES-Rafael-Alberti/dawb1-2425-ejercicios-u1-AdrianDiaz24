f2ad541a07a6bfc67d2dc8761acbf61905d1f3fa
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-20 00:07:58 +0200

    Ej. 1.2.19

---
+++ src/Ej12_19.py
@@ -0,0 +1,27 @@
+# Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca 
+# muestre por pantalla "NOMBRE tiene n letras.", donde NOMBRE es el nombre de usuario en mayúsculas y n es el número 
+# de letras que tienen el nombre.
+
+
+def pedir_nombre():
+    nombre = str(input("Dame tu nombre completo: "))
+    return nombre
+
+
+
+def nombre_mayus(nombre:str):
+    nombre_m = nombre.upper()
+    return nombre_m
+
+
+def contar_letras(nombre:str):
+    num_letras = nombre.count()
+    return num_letras
+
+
+def main():
+    nombre = pedir_nombre()
+    nombre_m = nombre_mayus(nombre)
+    a = contar_letras(nombre)
+    print(f"{nombre_m} tiene {a} letras.")
+    return 0
