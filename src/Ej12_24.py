268929819cfe22728ef08b9a46b77e1db759979e
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-21 00:23:07 +0200

    Ej. 1.2.24

---
+++ src/Ej12_24.py
@@ -0,0 +1,26 @@
+# Escribir un programa que pregunte por consola el precio de un producto en euros 
+# con dos decimales y muestre por pantalla el número de euros y el número de 
+# céntimos del precio introducido.
+
+
+def pedir_precio():
+    precio = float(input("Dame el percio de producto siguiendo el siguiente formato (x.xx)"))
+    return precio
+
+
+def separar_precio_e(precio):
+
+    return euros
+
+
+def separar_precio_c(precio):
+
+    return centimos
+
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
