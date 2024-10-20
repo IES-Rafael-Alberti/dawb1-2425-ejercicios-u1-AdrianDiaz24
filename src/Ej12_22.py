8d80579d828ae4b739b07688f17f6c8a1646ae55
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-20 21:44:58 +0200

    Ej. 1.2.22

---
+++ src/Ej12_22.py
@@ -0,0 +1,25 @@
+# Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, y después muestre 
+# por pantalla la misma frase pero con la vocal introducida en mayúscula.
+
+
+def pedir_frase():
+    frase = str(input("Introduce una frase: "))
+    return frase
+
+
+def pedir_vocal():
+    vocal = str(input("Introduce una vocal que volver mayusculas: "))
+    return vocal
+
+
+def convertir_frase():
+    
+    return frase_c
+
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
