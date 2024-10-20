a8cb4536b995bf85ff063c08ed78b526e251f4f3
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-20 04:11:17 +0200

    Ej. 1.2.21

---
+++ src/Ej12_21.py
@@ -0,0 +1,14 @@
+#Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
+
+
+def pedir_frase():
+    frase = str(input("Dime una frase que invertir: "))
+    return frase
+
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
