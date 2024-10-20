4345a762fea4099630344e34e2e9e2031bb064cf
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-20 22:34:35 +0200

    Ej. 1.2.22

--- src/Ej12_22.py
+++ src/Ej12_22.py
@@ -12,13 +12,29 @@ def pedir_vocal():
     return vocal
 
 
-def convertir_frase():
-    
+def convertir_frase(frase:str, vocal:str):
+    x = len(frase)
+    frase_c = str("")
+    i = 0
+    b = 0
+    while i < x:
+        y = frase[i]
+        if y == vocal:
+            frase_c = str(frase_c + frase[i].upper())
+            i = i+1
+        else:
+            frase_c = str(frase_c + frase[i])
+            i = i+1
+        b = b+1
     return frase_c
+    
 
 
 def main():
-
+    frase = pedir_frase()
+    vocal = pedir_vocal()
+    frase_c = convertir_frase(frase, vocal)
+    print(frase_c)
     return 0
 
 if __name__ == "__main__":
