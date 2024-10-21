22338cdbb88d2f21c1bc1a6650379d482751123d
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-21 20:51:36 +0200

    Ej. 1.2.27

---
+++ src/Ej12_26.py
@@ -0,0 +1,21 @@
+# Escribir un programa que pregunte por consola por los productos de una 
+# cesta de la compra, separados por comas, y muestre por pantalla cada uno de 
+# los productos en una línea distinta.
+
+
+def pedir_compra():
+    compra = str(input("Introduce los poductos de tu cesta de la compra, separa cada producto entre si por una coma: "))
+    return compra
+
+
+def separar_productos(compra):
+
+    return producto
+
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
