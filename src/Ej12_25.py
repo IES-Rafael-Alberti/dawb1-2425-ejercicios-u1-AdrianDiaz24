34c7dea00920de7a6d2f2048c16f221994208e45
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-21 18:16:59 +0200

    Ej. 1.2.25

--- src/Ej12_25.py
+++ src/Ej12_25.py
@@ -2,3 +2,8 @@
 # formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año. 
 # Adaptar el programa anterior para que también funcione cuando el día o el 
 # mes se introduzcan con un solo carácter.
+
+
+def pedir_fecha():
+    año = str(input("Introducaz su fecha de nacimiento, (siguiento este formato: dd/mm/aaaa): "))
+    return año
