efc0ecf1b1670b16d4307a83a386a2b0a1246c26
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-24 18:04:01 +0200

    Ej. 1.2.27

--- src/Ej12_27.py
+++ src/Ej12_27.py
@@ -1,5 +1,7 @@
-# Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre por pantalla una cadena con el nombre del producto 
-# seguido de su precio unitario con 6 dígitos enteros y 2 decimales, el número de unidades con tres dígitos y el coste total con 8 dígitos enteros y 2 decimales
+# Escribir un programa que pregunte el nombre el un producto, su precio y un 
+# número de unidades y muestre por pantalla una cadena con el nombre del producto 
+# seguido de su precio unitario con 6 dígitos enteros y 2 decimales, el número 
+# de unidades con tres dígitos y el coste total con 8 dígitos enteros y 2 decimales
 
 
 def precio_unitario(precio:str):
@@ -7,16 +9,11 @@ def precio_unitario(precio:str):
     centimos = ".00"
     precio_u = "000000.00"
     i = -1
-   
     if a == -1:
         precio = precio+centimos
     b = -1
-    precio = precio.rjust(9,"0")
+    precio_u = precio.rjust(9,"0")
 
-    for x in range(9):
-        precio_u = precio_u.replace(precio_u[i],precio[b])
-        i = i-1
-        b = b-1
     return precio_u
 
 
@@ -25,7 +22,7 @@ def main ():
     precio = str(input("Introduce el precio unitario: "))
     unidades = str(input("Introduce el nº de unidades disponibles: "))
     precio_u = precio_unitario(precio)
-    print(precio_u)
+    print(f"- {nombre} - {precio_u} - {unidades}")
     return 0
 
 if __name__ == "__main__":
